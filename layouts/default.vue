be675aec21b1a977425012435645338a82dd3401
Ganesh Gogate <ganesh.gogate@aventior.com>
2022-06-21 11:45:58 +0530

    My portfolio website first commit

---
+++ layouts/default.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: true,
+      drawer: true,
+      fixed: true,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        },
+        {
+          icon: 'mdi-cloud-download',
+          title: 'Download Resume',
+          to: '/'
+        }
+      ],
+      miniVariant: false,
+      title: 'Ganesh Gogate -Web Developer'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,202 @@
+<template>
+<div>
+    <!-- Personal details -->
+    <v-row justify="center" align="center">
+        <v-col cols="12" sm="10" md="10">
+            <v-card>
+                <v-card-title class="headline">
+                    Mr. Ganesh Narayan Gogate
+                </v-card-title>
+                <v-row>
+                    <v-col sm="10" md="5">
+                        <v-container>
+                            <img src="../static/GANESH.jpg" height="300" />
+                        </v-container>
+                    </v-col>
+                    <v-col sm="10" md="5">
+                        <v-container>
+                            <b>Address</b>
+                            <p>{{Address}}</p>
+                            <b>Age</b>
+                            <p>{{Age}}</p>
+                            <b>Email</b>
+                            <p>{{Email}}</p>
+                            <b>Phone</b>
+                            <p>+{{Phone}}</p>
+                            <b>Career Objective</b>
+                            <p>{{CareerObjective}}</p>
+                        </v-container>
+                    </v-col>
+                </v-row>
+            </v-card>
+        </v-col>
+    </v-row>
+    <!-- Education Details -->
+    <v-row justify="center" align="center">
+        <v-col cols="12" sm="10" md="10">
+            <v-card>
+                <v-card-title>
+                    Educational Details
+                </v-card-title>
+                <v-simple-table>
+                    <template v-slot:default>
+                        <thead>
+                            <tr>
+                                <th class="text-center">
+                                    Exam
+                                </th>
+                                <th class="text-center">
+                                    Year of passing
+                                </th>
+                                <th class="text-center">
+                                    Collage/Institution
+                                </th>
+                                <th class="text-center">
+                                    Percentage
+                                </th>
+                            </tr>
+
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>CDAC</td>
+                                <td>2021</td>
+                                <td>Know-IT</td>
+                                <td>69.83%</td>
+                            </tr>
+                            <tr>
+                                <td>BE(IT)</td>
+                                <td>2019</td>
+                                <td>D.K.T.E. Textile and EngineeringInstitute, Ichalkaranji</td>
+                                <td>65%</td>
+                            </tr>
+                            <tr>
+                                <td>Diploma&nbsp;</td>
+                                <td>2015</td>
+                                <td>DR.D.Y.Patil Polytechnic, kolhapur</td>
+                                <td>62%</td>
+                            </tr>
+                            <tr>
+                                <td>SSC</td>
+                                <td>2012</td>
+                                <td>Hanumantrao chate school, kolhapur</td>
+                                <td>72.40%</td>
+                            </tr>
+                        </tbody>
+                    </template>
+                </v-simple-table>
+            </v-card>
+        </v-col>
+    </v-row>
+    <!-- timeline -->
+    <v-row justify="center" align="center">
+        <v-col cols="12" sm="10" md="10">
+            <h2 class="text-center">Timeline</h2>
+            <template>
+                <v-timeline>
+                    <v-timeline-item v-for="(year, i) in years" :key="i" :color="year.color" small>
+                        <template v-slot:opposite>
+                            <span :class="`headline font-weight-bold ${year.color}--text`" v-text="year.year"></span>
+                        </template>
+                        <div class="">
+                            <h2 :class="`headline font-weight-light mb-4 ${year.color}--text`">
+                                {{year.title}}
+                            </h2>
+                            <div>
+                                {{year.desc}}
+                            </div>
+                        </div>
+                    </v-timeline-item>
+                </v-timeline>
+            </template>
+        </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+        <v-col cols="12" sm="10" md="10">
+            <v-card>
+                <v-card-title></v-card-title>
+            <v-row style="margin-left:20%">
+                <v-col cols="2">
+                    <img src="../static/GANESH.jpg" height="100" alt="">
+                </v-col>
+                <v-col cols="2">
+                    <img src="../static/GANESH.jpg" height="100" alt="">
+                </v-col>
+                <v-col cols="2">
+                    <img src="../static/GANESH.jpg" height="100" alt="">
+                </v-col>
+                <v-col cols="2">
+                    <img src="../static/GANESH.jpg" height="100" alt="">
+                </v-col>
+            </v-row>
+            </v-card>
+        </v-col>
+    </v-row>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'IndexPage',
+    data() {
+        return {
+            Address: null,
+            Age: null,
+            Phone: null,
+            Email: null,
+            CareerObjective: null,
+            years: [{
+                    color: 'amber',
+                    year: '2019',
+                    title: 'Graduation (BE)',
+                    desc: 'Completed graduation in information technology'
+                },
+                {
+                    color: 'blue',
+                    year: '2020',
+                    title: 'Ncircle Technology pvt ltd',
+                    desc: 'Worked as the member of technical staff'
+                },
+                {
+                    color: 'amber',
+                    year: '2021',
+                    title: 'CDAC',
+                    desc: 'Completed Post Graduate Diploma courses of C-DAC \n'
+                },
+                {
+                    color: 'blue',
+                    year: '2022',
+                    title: 'Aventior Digital pvt ltd',
+                    desc: 'Working as the frontend developer (VueJS Developer)'
+                },
+            ]
+        }
+    },
+    mounted() {
+        this.Address = process.env.Address;
+        this.Age = process.env.Age;
+        this.Phone = process.env.Number;
+        this.CareerObjective = process.env.CareerObjective;
+        this.Email = process.env.Email
+
+    }
+}
+</script>
+
+<style scoped>
+img {
+    border-radius: 10px;
+}
+
+#usedTech {
+    margin-top: 10rem;
+}
+
+td {
+    text-align: center;
+}
+
+v-col.img{
+    margin: 20px;
+}
+</style>
